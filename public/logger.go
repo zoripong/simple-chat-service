77ad5a97bc0de7e5941d5966b2c8ba552416d2ed
yool <devuri404@gmail.com>
2021-06-18 23:02:51 +0900

    feat: add GetInfoLogger

--- public/logger.go
+++ public/logger.go
@@ -12,3 +12,7 @@ type Logger struct {
 func GetWarningLogger() *Logger {
 	return &Logger{log.New(os.Stdout, "[WARNING] ", log.Ldate|log.Ltime|log.Lshortfile)}
 }
+
+func GetInfoLogger() *Logger {
+	return &Logger{log.New(os.Stdout, "[INFO] ", log.Ldate|log.Ltime|log.Lshortfile)}
+}
