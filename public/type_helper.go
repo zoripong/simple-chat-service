398f43f3ebd3313d6e6796ec080c428b2388de2d
yool <devuri404@gmail.com>
2021-06-19 18:00:59 +0900

    refactor: rename constant value

--- public/type_helper.go
+++ public/type_helper.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-const dateTimeFormat = "Mon Jan 2 15:04:05 -0700 MST 2006"
+const DATE_TIME_FORMAT = "Mon Jan 2 15:04:05 -0700 MST 2006"
 
 func ParseInt(target string) int {
 	result, err := strconv.Atoi(target)
@@ -16,7 +16,7 @@ func ParseInt(target string) int {
 }
 
 func ParseDatetime(target string) time.Time {
-	result, err := time.Parse(dateTimeFormat, target)
+	result, err := time.Parse(DATE_TIME_FORMAT, target)
 
 	if err != nil {
 		return time.Now()
@@ -25,5 +25,5 @@ func ParseDatetime(target string) time.Time {
 }
 
 func DatetimeToString(target *time.Time) string {
-	return target.Format(dateTimeFormat)
+	return target.Format(DATE_TIME_FORMAT)
 }
